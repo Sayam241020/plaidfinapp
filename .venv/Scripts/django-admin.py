1297d248726a7e5e6b0b4ebd09b158f839e9ff17
Sayam Agarwal <iitsayamagarwal24@gmail.com>
2022-10-29 12:09:23 +0530

    com

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\OneDrive\Desktop\new_project\Plaid_Django_Assignment\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
